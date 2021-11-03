a853031753f05ea2d47ea914439b2ad445ad7664
szwathub <szwathub@gmail.com>
2021-11-03 19:05:00 +0800

    feature(#29): finish `Trapping Rain Water`.

---
+++ Sources/LeetCode/42 Trapping Rain Water/TrappingRainWater.swift
@@ -0,0 +1,34 @@
+//
+//  File name: TrappingRainWater.swift
+//  Author:    Zhiwei Sun @szwathub
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      2021/11/3: Created by szwathub on 2021/11/3
+//
+
+import Foundation
+
+class TrappingRainWater {
+    func trap(_ height: [Int]) -> Int {
+        var left = 0, right = height.count - 1
+        var maxleft = 0, maxright = 0
+
+        var answer = 0
+        while left < right {
+            if height[left] < height[right] {
+                maxleft = max(maxleft, height[left])
+                answer += maxleft - height[left]
+                left += 1
+            } else {
+                maxright = max(maxright, height[right])
+                answer += maxright - height[right]
+                right -= 1
+            }
+        }
+
+        return answer
+    }
+}
