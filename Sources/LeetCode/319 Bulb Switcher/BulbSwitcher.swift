aafe4d5b45a779d11b286f1a2b5e32fabe61cac5
szwathub <szwathub@gmail.com>
2021-11-15 21:16:42 +0800

    feature(#50): finish `Bulb Switcher`.

---
+++ Sources/LeetCode/319 Bulb Switcher/BulbSwitcher.swift
@@ -0,0 +1,18 @@
+//
+//  File name: BulbSwitcher.swift
+//  Author:    Zhiwei Sun @szwathub
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      2021/11/15: Created by szwathub on 2021/11/15
+//
+
+import Foundation
+
+class BulbSwitcher {
+    func bulbSwitch(_ n: Int) -> Int {
+        return Int(sqrt(Double(n)))
+    }
+}
