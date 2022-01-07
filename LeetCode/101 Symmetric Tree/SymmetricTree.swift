e209240db5f9e552cb1dd670163aa78719fdb6a2
szwathub <szwathub@gmail.com>
2022-01-07 22:22:32 +0800

    feature(#110): finish `Symmetric Tree`.

--- LeetCode/101 Symmetric Tree/SymmetricTree.swift
+++ LeetCode/101 Symmetric Tree/SymmetricTree.swift
@@ -11,4 +11,36 @@
 //      2022/1/7: Created by szwathub on 2022/1/7
 //
 
-import Foundation
+import Structure
+
+class SymmetricTree {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return true
+        }
+
+        return check(root, root)
+    }
+
+    private func check(_ left: TreeNode?, _ right: TreeNode?) -> Bool {
+        if left == nil && right == nil {
+            return true
+        }
+
+        guard let left = left, let right = right else {
+            return false
+        }
+
+        if left.val != right.val {
+            return false
+        }
+        if check(left.left, right.right) == false {
+            return false
+        }
+        if check(left.right, right.left) == false {
+            return false
+        }
+
+        return true
+    }
+}
--- Tests/LeetCodeTests/101 Symmetric Tree/SymmetricTreeSpec.swift
+++ Tests/LeetCodeTests/101 Symmetric Tree/SymmetricTreeSpec.swift
@@ -1,38 +1,22 @@
-//
-//  Copyright © 2022 Zhiwei Sun. All rights reserved.
-//
-//  File name: SymmetricTreeSpec.swift
-//  Author:    Zhiwei Sun @szwathub
-//  E-mail:    szwathub@gmail.com
-//
-//  Description:
-//
-//  History:
-//      2022/1/7: Created by szwathub on 2022/1/7
-//
-
 import XCTest
+@testable import LeetCode
+@testable import Structure
 
-class SymmetricTreeSpec: XCTestCase {
+final class SymmetricTreeSpec: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    fileprivate let questions: [(([Int?]), Bool)] = [
+        (([1, 2, 2, 3, 4, 4, 3]), true),
+        (([1, 2, 3, 3, 4, 4, 3]), false),
+        (([1, 2, 2, nil, 3, nil, 3]), false),
+        (([1, 2, 2, 3, 3, nil, 3]), false),
+        (([]), true)
+    ]
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSymmetricTreeSpec() {
+        let solution = SymmetricTree()
+        for ((elements), answer) in questions {
+            let tree = BinaryTree(elements)
+            XCTAssertEqual(solution.isSymmetric(tree.root), answer)
         }
     }
-
 }
