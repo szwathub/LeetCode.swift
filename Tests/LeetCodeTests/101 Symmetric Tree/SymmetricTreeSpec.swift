c3b087df23a9fb2e3096c5be75c81c1f5f5c22c6
szwathub <szwathub@gmail.com>
2022-01-07 22:18:52 +0800

    feature(#110): finish `Symmetric Tree`.

---
+++ Tests/LeetCodeTests/101 Symmetric Tree/SymmetricTreeSpec.swift
@@ -0,0 +1,38 @@
+//
+//  Copyright © 2022 Zhiwei Sun. All rights reserved.
+//
+//  File name: SymmetricTreeSpec.swift
+//  Author:    Zhiwei Sun @szwathub
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      2022/1/7: Created by szwathub on 2022/1/7
+//
+
+import XCTest
+
+class SymmetricTreeSpec: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
