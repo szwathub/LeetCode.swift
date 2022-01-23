d3aa7d8a590eced37752d71274596098b522b3ac
szwathub <szwathub@gmail.com>
2022-01-23 22:16:23 +0800

    feature(#146): finish `Jump Game IV`.

--- LeetCode/1345 Jump Game IV/JumpGameIV.swift
+++ LeetCode/1345 Jump Game IV/JumpGameIV.swift
@@ -14,20 +14,23 @@
 class JumpGameIV {
     func minJumps(_ arr: [Int]) -> Int {
         var map = [Int: [Int]]() // value: index
-        for (index, value) in arr.enumerated() {
-            var list = map[value, default: []]
-            list.append(index)
-            map[value] = list
+        for index in 0..<arr.count {
+            if index - 1 >= 0 && index + 1 < arr.count,
+               arr[index - 1] == arr[index] && arr[index] == arr[index + 1] {
+                continue
+            }
+
+            map[arr[index], default: []].append(index)
         }
 
         var queue: [(Int, Int)] = [(0, 0)] // index: count
         var visited = Set<Int>()
         visited.insert(0)
 
-        var answer = 0
+        var answer = -1
         while !queue.isEmpty {
             var (index, step) = queue.removeFirst()
-            if step == arr.count - 1 {
+            if index == arr.count - 1 {
                 answer = step
                 break
             }
@@ -35,15 +38,21 @@ class JumpGameIV {
             let value = arr[index]
             step += 1
 
-            for valueIndex in map[value]! where !visited.contains(valueIndex) {
-                visited.insert(valueIndex)
-                queue.append((valueIndex, step))
+            if let list = map[value] {
+                for valueIndex in list where !visited.contains(valueIndex) {
+                    visited.insert(valueIndex)
+                    queue.append((valueIndex, step))
+                }
             }
 
             map.removeValue(forKey: value)
             if index + 1 < arr.count && !visited.contains(index + 1) {
                 visited.insert(index + 1)
                 queue.append((index + 1, step))
+            }
+            if index - 1 >= 0 && !visited.contains(index - 1) {
+                visited.insert(index - 1)
+                queue.append((index - 1, step))
             }
         }
 
