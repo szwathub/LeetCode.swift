5e8dcdf40e38a7a93c8f3c90f1622a01d811d6d8
szwathub <szwathub@gmail.com>
2022-03-14 22:20:30 +0800

    fixes.

--- Tests/LeetCodeTests/599 Minimum Index Sum of Two Lists/MinimumIndexSumofTwoListsSpec.swift
+++ Tests/LeetCodeTests/599 Minimum Index Sum of Two Lists/MinimumIndexSumofTwoListsSpec.swift
@@ -4,9 +4,11 @@ import XCTest
 final class MinimumIndexSumofTwoListsSpec: XCTestCase {
 
     fileprivate let questions: [(([String], [String]), [String])] = [
-        ((["Shogun", "Tapioca Express", "Burger King", "KFC"], ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]), ["Shogun"]),
+        ((["Shogun", "Tapioca Express", "Burger King", "KFC"],
+          ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]), ["Shogun"]),
         ((["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Shogun", "Burger King"]), ["Shogun"]),
-        ((["Shogun","Tapioca Express","Burger King","KFC"], ["KFC","Burger King","Tapioca Express","Shogun"]), ["KFC","Burger King","Tapioca Express","Shogun"])
+        ((["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Burger King", "Tapioca Express", "Shogun"]),
+         ["KFC", "Burger King", "Tapioca Express", "Shogun"])
     ]
 
     func testMinimumIndexSumofTwoLists() {
