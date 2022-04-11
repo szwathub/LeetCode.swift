ffb0ca180259f125f19d85c770805b71668ad2a0
codefactor-io <support@codefactor.io>
2022-04-11 15:41:14 +0000

    [CodeFactor] Apply fixes

--- Tests/LeetCodeTests/2100 Find Good Days to Rob the Bank/FindGoodDaystoRobtheBankSpec.swift
+++ Tests/LeetCodeTests/2100 Find Good Days to Rob the Bank/FindGoodDaystoRobtheBankSpec.swift
@@ -5,7 +5,7 @@ final class FindGoodDaystoRobtheBankSpec: XCTestCase {
 
     fileprivate let questions: [(([Int], Int), [Int])] = [
         (([5, 3, 3, 3, 5, 6, 2], 2), [2, 3]),
-        (([1, 1, 1, 1, 1], 0), [0, 1, 2, 3,4]),
+        (([1, 1, 1, 1, 1], 0), [0, 1, 2, 3, 4]),
         (([1, 2, 3, 4, 5, 6], 2), []),
         (([1], 5), []),
         (([0, 0, 0, 0, 0], 3), [])
