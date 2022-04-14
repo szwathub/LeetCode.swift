41f30a2848a2eb02f97cf303bda90c67b49d9399
codefactor-io <support@codefactor.io>
2022-04-14 10:49:58 +0000

    [CodeFactor] Apply fixes

--- Tests/LeetCodeTests/1672 Richest Customer Wealth/RichestCustomerWealthSpec.swift
+++ Tests/LeetCodeTests/1672 Richest Customer Wealth/RichestCustomerWealthSpec.swift
@@ -5,7 +5,7 @@ final class RichestCustomerWealthSpec: XCTestCase {
 
     fileprivate let questions: [(([[Int]]), Int)] = [
         (([[1, 2, 3], [3, 2, 1]]), 6),
-        (([[1, 5], [7, 3], [3,5]]), 10)
+        (([[1, 5], [7, 3], [3, 5]]), 10)
     ]
 
     func testRichestCustomerWealthSpec() {
