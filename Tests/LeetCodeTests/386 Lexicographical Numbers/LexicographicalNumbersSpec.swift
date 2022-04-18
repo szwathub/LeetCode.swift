483732f0b272135cd445e0969b16112c03e253d7
codefactor-io <support@codefactor.io>
2022-04-18 08:54:38 +0000

    [CodeFactor] Apply fixes

--- Tests/LeetCodeTests/386 Lexicographical Numbers/LexicographicalNumbersSpec.swift
+++ Tests/LeetCodeTests/386 Lexicographical Numbers/LexicographicalNumbersSpec.swift
@@ -5,7 +5,7 @@ import XCTest
 final class LexicographicalNumbersSpec: XCTestCase {
 
     fileprivate let questions: [((Int), [Int])] = [
-        ((13), [1,10,11,12,13,2,3,4,5,6,7,8,9]),
+        ((13), [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]),
         ((2), [1, 2])
     ]
 
